3e075d01e7548833914c585a8494e3abdfb4ed7f
liuheng <liuheng0315@gmail.com>
2018-04-12 21:58:23 +0800

    格式修改

--- src/offerLearning/MoreThanHalfNum_Solution.java
+++ src/offerLearning/MoreThanHalfNum_Solution.java
@@ -5,26 +5,27 @@ package offerLearning;
  */
 public class MoreThanHalfNum_Solution {
     public static void main(String[] args) {
-        moreThanHalfNum(new int[]{1,2,3,2,2,2,5,4,2});
+        moreThanHalfNum(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2});
     }
-    public static int moreThanHalfNum(int[] array){
-        if (array.length==0){
+
+    public static int moreThanHalfNum(int[] array) {
+        if (array.length == 0) {
             return 0;
         }
-        int i=1;
-        int result=array[0];
+        int i = 1;
+        int result = array[0];
         for (int j = 1; j < array.length; j++) {
             if (i == 0) {
-                result=array[j];
-                i=1;
+                result = array[j];
+                i = 1;
             } else if (result == array[j]) {
                 i++;
-            }else {
+            } else {
                 i--;
             }
         }
-        int count =0;
-        for (int j = 0; j <array.length ; j++) {
+        int count = 0;
+        for (int j = 0; j < array.length; j++) {
             if (result == array[j]) {
                 count++;
             }
