0f3d2e25e391e7044e97a52a21e788e1f0da50fa
liuheng <liuheng0315@gmail.com>
2018-04-15 15:34:00 +0800

    给目标数，在数组中查找两个数，返回数组的两个角标

--- src/offerLearning/FindNumsAppearOnce.java
+++ src/offerLearning/FindNumsAppearOnce.java
@@ -2,4 +2,11 @@ package offerLearning;
 
 public class FindNumsAppearOnce {
     //查找数组中只出现一次的数字
+    public static void main(String[] args) {
+
+    }
+
+    public void findNumsAppearOnce(int[] array, int[] num1, int[] num2) {
+
+    }
 }
