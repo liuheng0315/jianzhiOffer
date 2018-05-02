d3771a512ad1b89b12991867489bd9fa5423940e
liuheng <liuheng0315@gmail.com>
2018-05-02 14:37:36 +0800

    数组中重复的字符串

---
+++ src/offerLearning/Duplicate.java
@@ -0,0 +1,25 @@
+package offerLearning;
+
+import java.util.HashSet;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        Duplicate duplicate = new Duplicate();
+        boolean b = duplicate.duplicate(new int[]{}, 0, new int[]{});
+    }
+    //字符串中的字符是否重复
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if (numbers.length == 0||numbers==null) {
+            duplication[0] = -1;
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < length; i++) {
+            if (!set.add(numbers[i])) {
+                duplication[0] = numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
+}
